08471e0018b8096bba854389510f0b53476ecb7a
gregral-ban <janoslegradi183@gmail.com>
2026-07-14 12:46:51 +0000

    Destruktor 46:27

--- osztalyok/main.cpp
+++ osztalyok/main.cpp
@@ -10,7 +10,7 @@ namespace Tarolo
     class Szamok
     {
         double* tomb = nullptr;
-        
+
         /*
         *private:
         //...
@@ -27,17 +27,30 @@ namespace Tarolo
         * Ezeket akár lehet priváttá tenni,
         */
         unsigned meret = 0;
-    
+
         public:
             // Szamok()
             // {
             //     std::cout << ">> Konstruktor letrejon <<" << std::endl;
             // }
 
-            Szamok(unsigned meret)
+            // Szamok(unsigned pmeret)
+            // {
+            //     meret = pmeret;
+            //     tomb = new double[meret];
+            // }
+
+            Szamok(unsigned pmeret) :
+                meret(pmeret),
+                tomb(new double[pmeret])
             {
-                this->meret = meret;
-                this->tomb = new double[meret];
+            }
+            // Destruktor
+            // ~[Osztály neve]
+            // Paraméter nélküli függvény
+            ~Szamok()
+            {
+                delete[] tomb;
             }
 
             unsigned darabLeker()
@@ -56,11 +69,14 @@ int main()
 {
     // Tarolo::Szamok s1;
     // Tarolo::Szamok s2;
-    
+
     Tarolo::Szamok s1(4);
     Tarolo::Szamok s2(8);
     std::cout << s1.darabLeker() << std::endl; // Visszaadja a 'meret'-et.
     std::cout << s2.darabLeker() << std::endl;
     // Tarolo::darabLeker(s);
+
+    // Tarolo::Szamok* p = new Tarolo::Szamok(10);
+    // delete *p;
     return 0;
 }
